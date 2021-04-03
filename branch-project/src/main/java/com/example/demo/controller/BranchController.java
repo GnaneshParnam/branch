eeb53ddae07f086c8019d5d9710c54876d5b8399
GnaneshParnam <80418198+GnaneshParnam@users.noreply.github.com>
2021-04-03 10:33:11 +0530

    Add files via upload

---
+++ branch-project/src/main/java/com/example/demo/controller/BranchController.java
@@ -0,0 +1,58 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.Branch;
+import com.example.demo.service.BranchService;
+
+@RestController
+@RequestMapping("/branch")
+@CrossOrigin(origins = {"http://localhost:4200", "*"})
+public class BranchController {
+	@Autowired
+	BranchService bs;
+	
+	@GetMapping("/")
+	public List<Branch> getAllBranches()
+	{
+		//display all branches
+		List<Branch> branches = bs.read();
+		return branches;
+	}
+	
+	@GetMapping("/{bid}")			//?bid=B00017 means @RequestParam		but /{bid} means @PathVariable
+	public Branch findBranchById(@PathVariable("bid") String bid)
+	{
+		return bs.read(bid);
+	}
+	
+	@PostMapping("/")
+	public Branch addBranch(@RequestBody Branch branch)
+	{
+		return bs.create(branch);
+	}
+	
+	@PutMapping("/")
+	public Branch modifyBranch(@RequestBody Branch branch)
+	{
+		return bs.update(branch);
+	}
+	
+	@DeleteMapping("/{bid}")
+	public void deleteBranch(@PathVariable("bid") String bid)
+	{
+		bs.delete(bid);
+	}
+
+}
